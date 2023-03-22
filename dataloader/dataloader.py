9efea0e5b74c2c82f2282a5e424a33caa9907e18
junyuyang7 <1054342975@qq.com>
2023-03-22 13:53:42 +0900

    0322

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -1,10 +1,16 @@
 from os import P_WAIT
 import numpy as np
 from itertools import combinations
+from query import QuerySelector, BaseQuerySelector, GreedyQuerySelector, RandomQuerySelector
+from worker import Worker, WorkerFactory
 from fact import FactSet
 from typing import Tuple, List
 import abc
 import itertools
+from param import param
+import pandas as pd
+
+source = './dataset/'
 
 
 class DataLoader(metaclass=abc.ABCMeta):
@@ -40,6 +46,7 @@ class NormalDataloader(DataLoader):
         self.primary_len = len(self.idx)
         self.dict_i = {}  # hwx:统计次数
 
+
     def __getitem__(self, item):
         """
         返回mean_entropy的方法为最简单的每轮计算一次
@@ -63,11 +70,13 @@ class NormalDataloader(DataLoader):
             time += 1
         return self.idx[item], self.prior_p[item], self.ground_truth[item], sum
 
+
     def add_data(self, idx, post_p, gt):
         self.idx.append(idx)
         self.prior_p[len(self.prior_p) % self.primary_len] = post_p
         self.prior_p.append(post_p)
         self.ground_truth.append(gt)
+
 
     def wirte_post_p_to_txt(self, txt_name, output_file):
         """
@@ -110,12 +119,14 @@ class IncreEntropyDataloader(DataLoader):
         self.prior_p = prior_p
         self.ground_truth = ground_truth
         self.entropy_list = None
-    
+
+
     def get_subset(mylist, k):
         val = []
         for i in itertools.combinations(mylist, k):
             val.append(list(i))
         return val
+
 
     def __getitem__(self, item):
         """
@@ -143,10 +154,12 @@ class IncreEntropyDataloader(DataLoader):
         del self.ground_truth[self.entropy_list[0]]
         return return_i, return_p, return_g, sum
 
+
     def add_data(self, idx, post_p, gt):
         self.idx.append(idx)
         self.prior_p.append(post_p)
         self.ground_truth.append(gt)
+
 
     def wirte_post_p_to_txt(self, txt_name, output_file):
         """
@@ -167,6 +180,7 @@ class IncreEntropyDataloader(DataLoader):
                     f.write(' ')
                 f.write('\n')
 
+
 class BruteChoiceDataloader(DataLoader):
     """
     数据存储器——暴力法
@@ -175,7 +189,7 @@ class BruteChoiceDataloader(DataLoader):
     def __init__(self, idx: List[np.ndarray],
                  prior_p: List[np.ndarray],
                  ground_truth: List[np.ndarray],
-                 k: int):
+                 k: int,):
         """
         :param idx: 数据的id
         :param prior_p: 数据的先验概率
@@ -186,8 +200,64 @@ class BruteChoiceDataloader(DataLoader):
         self.prior_p = prior_p
         self.ground_truth = ground_truth
         self.entropy_list = None
-        self.k = k
+        self.k = k # 选择任务的数量
         self.dict_i = {}  # hwx:统计次数
+        self.query_per_iteration = 1
+        self.acc_list = []
+        self.topic = {}
+        self.w_labels = {}
+        self.acc_dic = {}
+
+
+    def get_topic(self):
+        df = pd.read_csv('./dataset/' + param.dataname + '_' + str(param.deta) + '_' + str(param.low) + '_theta' + str(
+            param.theta) + '/Preliminary_processing_data1_Output/' + param.dataname + '_id_topic.csv')
+        id_list = df['id'].tolist()
+        topic_list = df['topic'].tolist()
+        for i in range(len(id_list)):
+            self.topic[str(id_list[i])] = topic_list[i]
+        return self.topic
+
+
+    def get_acclist(self, id):
+        with open('./dataset/' + param.dataname + '_' + str(param.deta) + '_' + str(param.low) + '_theta' + str(param.theta) + '/Preliminary_processing_data1_Output/' + param.dataname + '_worker_acc'+str(param.deta)+'_'+str(param.low)+ '_theta' + str(param.theta) +'.txt', 'r') as f:
+            raw_lines = f.readlines()
+            i = 0
+            while i != len(raw_lines):
+                idx = raw_lines[i].replace('\n', '')
+                i += 1
+                p = raw_lines[i].strip().strip('[').strip(']').strip(',').split('] [')
+                p_list = []
+                for p_topic in p:
+                    p_topic_i = p_topic.split(', ')
+                    p_list.append(p_topic_i)
+
+                self.acc_dic[idx] = p_list
+                i += 1
+                # 修改成列表
+                answer_list = []
+                for j in range(len(p)):
+                    answer_list.append(raw_lines[i].strip().split(' '))
+                    i += 1
+                self.w_labels[idx] = answer_list
+
+        expert_num = len(self.w_labels[str(id[0])])
+        f_topic = []
+        worker_accuracy_topic = []
+        worker_accuracy = self.acc_dic[str(id[0])]
+        for acc_topic in worker_accuracy:
+            worker_accuracy_topic.append([float(i) for i in acc_topic])
+        for i in range(param.group_num):
+            if id[0] + i >= param.get_length(source):
+                break
+            f_topic.append(self.topic[str(id[0] + i)])
+        for i in range(expert_num):
+            worker_acc = []
+            for topic in f_topic:
+                worker_acc.append(worker_accuracy_topic[i][topic])
+        self.acc_list = np.array(worker_acc)
+        self.acc_list = np.expand_dims(self.acc_list, axis=0)
+
 
     def get_subset(self, mylist, k):
         val = []
@@ -195,47 +265,74 @@ class BruteChoiceDataloader(DataLoader):
             val.append(list(i))
         return val
 
+
     def __getitem__(self, item):
         tmp = [] # tmp = [task1权重 task2权重 ... task722权重]
         sum = 0 # sum 为总熵值
-        for i, p, g in zip(self.idx, self.prior_p, self.ground_truth):
+        topic = self.get_topic()
+        for ix, p, g in zip(self.idx, self.prior_p, self.ground_truth):
+            # print('ix:' , ix)
             p = p.tolist()
-            g = g.tolist()
             if len(p) > 1:
                 p = [i if i-0 > 1e-5 else 1e-5 for i in p]
                 p_t = np.array(p)
             else:
                 p_t = np.array([1 - p[0] if 1 - p[0] != 0 else 1e-5, p[0]])
-            # 修改权重部分
-            if p_t.size < self.k:
-                tmp.append(np.sum(-p_t * np.log2(p_t)).item())
-            else:
-                # 所有可能的序列逐个遍历找到质量最好的
-                combis = self.get_subset(range(p_t.size), k=self.k)
-                max_combi_h = 0
-                max_combi = []
-                for combi in combis:
-                    p_t_combi = p_t[np.array(combi)]
-                    p_t_combi_h = np.sum(-p_t_combi * np.log2(p_t_combi)).item()
-                    if p_t_combi_h > max_combi_h:
-                        max_combi = p_t_combi
-                        max_combi_h = p_t_combi_h
-                tmp.append(max_combi_h)
-            #
+            # 所有可能的序列逐个遍历找到质量最好的
+            # change--------------------------------------------------
+            tmp_factset = []
+            tmp_factspace = []
+            s_format = '{:0' + str(len(g)) + 'b}'
+            factset_len = len(p_t)
+            for i in range(factset_len):
+                s = s_format.format(i)
+                s = list(s)
+                tmp_in = []
+                for j in range(len(s)):
+                    tmp_in.append(int(s[j]))
+                tmp_factset.append(tmp_in)
+            for i in range(factset_len):
+                tmp_factspace.append([0, 1])
+
+
+            gt_str = [str(i) for i in g]
+            gt_str.reverse()
+            gt_str = ''.join(gt_str)
+            # print(gt_str)
+            ground_true_int = int(gt_str, 2)
+            facts = FactSet(np.array(tmp_factset),
+                              prior_p=np.array(p_t),
+                              ground_true=ground_true_int,
+                              fact_space=tmp_factspace
+                              )
+            query_selector = BaseQuerySelector()
+            self.get_acclist(ix)
+            # print('self.acc_list', self.acc_list)
+            query_idxes, _, max_h = query_selector.select(facts,
+                                           self.k,
+                                           self.acc_list)
+            # print('query_idxes: ',query_idxes)
+            tmp.append(max_h)
+
             sum += np.sum(-p_t * np.log2(p_t)).item()
-        self.entropy_list = sorted(range(len(tmp)), key=lambda k: -tmp[k])
-        # return_i, return_p, return_g = self.idx[self.entropy_list[0]], self.prior_p[self.entropy_list[0]], self.ground_truth[self.entropy_list[0]]
-        # del self.idx[self.entropy_list[0]]
-        # del self.prior_p[self.entropy_list[0]]
-        # del self.ground_truth[self.entropy_list[0]]
+            # tmp.append(np.sum(-p_t * np.log2(p_t)).item())
+        print('tmp: ', tmp)
+        self.entropy_list = sorted(range(len(tmp)), key=lambda a: -tmp[a])
+        print('self.entropy_list: ', self.entropy_list)
+        # print('idx: ', self.idx)
+        # print('k, len(entropy_list):', self.k, len(self.entropy_list))
+
         i = 0
-        # print(self.idx[self.entropy_list[i]][0])
         flag = 1
         while flag == 1:
             if self.idx[self.entropy_list[i]][0] not in self.dict_i:
                 self.dict_i[self.idx[self.entropy_list[i]][0]] = 1
                 flag = 0
-            elif self.dict_i[self.idx[self.entropy_list[i]][0]] >= 7:
+            # yjy:控制挑选次数
+            elif self.dict_i[self.idx[self.entropy_list[i]][0]] >= (param.tim * param.group_num) or self.dict_i[
+                self.idx[self.entropy_list[i]][0]] - min(self.dict_i.values()) >= (param.tim * param.group_num // 2+(param.group_num-self.k)):
+            # elif self.dict_i[self.idx[self.entropy_list[i]][0]] >= (param.tim * param.group_num) or self.dict_i[
+            #     self.idx[self.entropy_list[i]][0]] - min(self.dict_i.values()) > 2:
                 i += 1
             else:
                 self.dict_i[self.idx[self.entropy_list[i]][0]] += 1
@@ -247,12 +344,15 @@ class BruteChoiceDataloader(DataLoader):
         del self.idx[self.entropy_list[i]]
         del self.prior_p[self.entropy_list[i]]
         del self.ground_truth[self.entropy_list[i]]
+        print('dict_i: ', self.dict_i)
         return return_i, return_p, return_g, sum
+
 
     def add_data(self, idx, post_p, gt):
         self.idx.append(idx)
         self.prior_p.append(post_p)
         self.ground_truth.append(gt)
+
 
     def wirte_post_p_to_txt(self, txt_name, output_file):
         """
@@ -285,21 +385,87 @@ class ApproxChoiceDataloader(DataLoader):
                  k: int):
         """
         :param idx: 数据的id
-        :param prior_p: 数据的先验概率
+        :param prior_p: 数据的先验概率(2^n)
         :param ground_truth: 数据的ground_truth
         :param entropy_list: 熵值从高到底的item的index
         """
         self.idx = idx
         self.prior_p = prior_p
         self.ground_truth = ground_truth
+
+        self.idx1 = idx
+        self.prior_p1 = prior_p
+        self.ground_truth1 = ground_truth
+
         self.entropy_list = None
         self.k = k
         self.dict_i = {} #hwx:统计次数
+        self.query_per_iteration = 1
+        self.acc_list = []
+        self.topic = {}
+        self.w_labels = {}
+        self.acc_dic = {}
+
+
+    def get_topic(self):
+        df = pd.read_csv('./dataset/' + param.dataname + '_' + str(param.deta) + '_' + str(param.low) + '_theta' + str(
+            param.theta) + '/Preliminary_processing_data1_Output/' + param.dataname + '_id_topic.csv')
+        id_list = df['id'].tolist()
+        topic_list = df['topic'].tolist()
+        for i in range(len(id_list)):
+            self.topic[str(id_list[i])] = topic_list[i]
+        return self.topic
+
+
+    def get_acclist(self, id):
+        with open('./dataset/' + param.dataname + '_' + str(param.deta) + '_' + str(param.low) + '_theta' + str(param.theta) + '/Preliminary_processing_data1_Output/' + param.dataname + '_worker_acc'+str(param.deta)+'_'+str(param.low)+ '_theta' + str(param.theta) +'.txt', 'r') as f:
+            raw_lines = f.readlines()
+            i = 0
+            while i != len(raw_lines):
+                idx = raw_lines[i].replace('\n', '')
+                i += 1
+                p = raw_lines[i].strip().strip('[').strip(']').strip(',').split('] [')
+                p_list = []
+                for p_topic in p:
+                    p_topic_i = p_topic.split(', ')
+                    p_list.append(p_topic_i)
+
+                self.acc_dic[idx] = p_list
+                i += 1
+                # 修改成列表
+                answer_list = []
+                for j in range(len(p)):
+                    answer_list.append(raw_lines[i].strip().split(' '))
+                    i += 1
+                self.w_labels[idx] = answer_list
+        expert_num = len(self.w_labels[str(id[0])])
+
+        f_topic = []
+        worker_accuracy_topic = []
+        worker_accuracy = self.acc_dic[str(id[0])]
+        for acc_topic in worker_accuracy:
+            worker_accuracy_topic.append([float(i) for i in acc_topic])
+        for i in range(param.group_num):
+            if id[0] + i >= param.get_length(source):
+                break
+            f_topic.append(self.topic[str(id[0] + i)])
+        for i in range(expert_num):
+            worker_acc = []
+            for topic in f_topic:
+                worker_acc.append(worker_accuracy_topic[i][topic])
+        self.acc_list = np.array(worker_acc)
+        self.acc_list = np.expand_dims(self.acc_list, axis=0)
+
+
+    def get_facset(self, fact):
+        self.facts = fact
+
 
     def __getitem__(self, item):
         tmp = []  # tmp = [task1权重 task2权重 ... task722权重]
-        sum = 0  # sum 为总熵值
-        for i, p, g in zip(self.idx, self.prior_p, self.ground_truth):
+        sum_h = 0  # sum 为总熵值
+        topic = self.get_topic()
+        for ix, p, g in zip(self.idx, self.prior_p, self.ground_truth):
             p = p.tolist()
             g = g.tolist()
             if len(p) > 1:
@@ -307,36 +473,71 @@ class ApproxChoiceDataloader(DataLoader):
                 p_t = np.array(p)
             else:
                 p_t = np.array([1 - p[0] if 1 - p[0] != 0 else 1e-5, p[0]])
-            # 修改权重部分
-            if p_t.size < self.k:
-                tmp.append(np.sum(-p_t * np.log2(p_t)).item())
-            else:
-                max_choice = []
-                while len(max_choice) != self.k:
-                    max_select_fact = 0
-                    max_select_fact_h = 0
-                    for index in range(p_t.size):
-                        if index in max_choice:
-                            continue
-                        h_now = np.sum(-p_t[index] * np.log2(p_t[index])).item()
-                        if h_now > max_select_fact_h:
-                            max_select_fact_h = h_now
-                            max_select_fact = index
-                    max_choice.append(max_select_fact)
-                tmp.append(np.sum(-p_t[np.array(max_choice)] * np.log2(p_t[np.array(max_choice)])).item())
 
-            sum += np.sum(-p_t * np.log2(p_t)).item()
-        self.entropy_list = sorted(range(len(tmp)), key=lambda k: -tmp[k])
+            # 修改权重部分
+            # change--------------------------------------------------
+            tmp_factset = []
+            tmp_factspace = []
+            s_format = '{:0' + str(len(g)) + 'b}'
+            factset_len = len(p_t)
+            for i in range(factset_len):
+                s = s_format.format(i)
+                s = list(s)
+                tmp_in = []
+                for j in range(len(s)):
+                    tmp_in.append(int(s[j]))
+                tmp_factset.append(tmp_in)
+            for i in range(factset_len):
+                tmp_factspace.append([0, 1])
+
+            gt_str = [str(i) for i in g]
+            gt_str.reverse()
+            gt_str = ''.join(gt_str)
+            # print(gt_str)
+            ground_true_int = int(gt_str, 2)
+            self.facts = FactSet(np.array(tmp_factset),
+                            prior_p=np.array(p_t),
+                            ground_true=ground_true_int,
+                            fact_space=tmp_factspace
+                            )
+            query_selector = GreedyQuerySelector()
+            self.get_acclist(ix)
+            # print('self.acc_list', self.acc_list)
+
+            # --------------------------------------------------
+
+            query_idxes, _, max_h = query_selector.select(self.facts,
+                                                          self.k,
+                                                          self.acc_list)
+            # print('query_idxes: ', query_idxes)
+            # print('max_h: ', max_h)
+            tmp.append(max_h)
+
+            sum_h += np.sum(-p_t * np.log2(p_t)).item()
+
+        print('tmp:', tmp)
+        # print('len(tmp): ', len(tmp))
+        self.entropy_list = sorted(range(len(tmp)), key=lambda a: -tmp[a])
+        print('self.entropy_list: ', self.entropy_list)
+        # print('k, len(entropy_list):', self.k, len(self.entropy_list))
 
         # hwx:统计次数,不超过3次
+        if self.k >=3:
+            max_sel = 1
+        else:
+            max_sel = (param.tim * param.group_num // 2+(param.group_num-self.k))
         i= 0
-        # print(self.idx[self.entropy_list[i]][0])
+        print(self.idx[self.entropy_list[i]][0])
         flag = 1
         while flag == 1:
             if self.idx[self.entropy_list[i]][0] not in self.dict_i:
                 self.dict_i[self.idx[self.entropy_list[i]][0]] = 1
                 flag = 0
-            elif self.dict_i[self.idx[self.entropy_list[i]][0]] >= 7:
+            # yjy:控制挑选次数
+            # elif self.dict_i[self.idx[self.entropy_list[i]][0]] >= (param.tim * param.group_num) or self.dict_i[
+            #     self.idx[self.entropy_list[i]][0]] - min(self.dict_i.values()) >= (param.tim * param.group_num // 2+(param.group_num-self.k)):
+            elif self.dict_i[self.idx[self.entropy_list[i]][0]] >= (param.tim * param.group_num) or self.dict_i[
+                self.idx[self.entropy_list[i]][0]] - min(self.dict_i.values()) >= max_sel:
                 i += 1
             else:
                 self.dict_i[self.idx[self.entropy_list[i]][0]] += 1
@@ -344,12 +545,14 @@ class ApproxChoiceDataloader(DataLoader):
 
         return_i, return_p, return_g = self.idx[self.entropy_list[i]], self.prior_p[self.entropy_list[i]], \
                                        self.ground_truth[self.entropy_list[i]]
-        # print(return_i)
+
+
         del self.idx[self.entropy_list[i]]
         del self.prior_p[self.entropy_list[i]]
         del self.ground_truth[self.entropy_list[i]]
+        print('dict_i: ', self.dict_i)
 
-        return return_i, return_p, return_g, sum
+        return return_i, return_p, return_g, sum_h
 
     def add_data(self, idx, post_p, gt):
         self.idx.append(idx)
--- query/query.py
+++ query/query.py
@@ -2,11 +2,11 @@ import imp
 import numpy as np
 from itertools import combinations
 
-from regex import subf
+# from regex import subf
 from fact import FactSet
 from typing import Tuple
 import abc
-import copy
+import random
 
 
 class QuerySelector(metaclass=abc.ABCMeta):
@@ -29,29 +29,51 @@ class BaseQuerySelector(QuerySelector):
     暴力法的问题选择器
     """
 
-    def select(self, facts: FactSet,
-               num: int,
-               worker_accuracy: np.ndarray
-               ) -> Tuple[np.ndarray, "FactSet", float]:
+    def select(self, facts: FactSet, num: int, worker_accuracy: np.ndarray) -> Tuple[np.ndarray, "FactSet", float]:
         num_fact: int = facts.num_fact()
+        if num > num_fact:  # k等于5，但fact只有4
+            num = num_fact
         assert num <= num_fact
+        # print('num: ', num)
         selections = combinations(range(num_fact), num)
-        max_h: float = -float('inf')
+        # print('worker_accuracy: ', worker_accuracy)
+        # max_h: float = -float('inf')
+        max_h = 0.
         max_selection: Tuple[int] = (0,) * num_fact
         for selection in selections:
             sub_facts = facts.get_subset(list(selection))
+            # h = 0.
+            prior_p = sub_facts.get_prior_p()  # 初始化prior_p
             h = 0.
-            # 计算answer set的墒
+            for i in prior_p:
+                if i == 0:  # 有些为0的 不好算log
+                    continue
+                h -= (i * np.log(i)).item()  # H(O)
+            # 暴力枚举并找到fact最优组合
+            h_gain = 0.
             for i in range(len(sub_facts)):
-                # print("sub_facts",sub_facts[i])
-                p_ans, _ = sub_facts.compute_ans_p(sub_facts[i],
+                cur_h = 0.
+                # 传进去sub_facts[i]，相当于一个 o --- (当CE答案为sub_facts[i]时，获得P(ATCE) 和 P(ATCE|o))
+                ans_p, ans_p_post_o = sub_facts.compute_ans_p(sub_facts[i],
                                                 list(range(sub_facts.num_fact())),
                                                 worker_accuracy[:,list(selection)])
-                h -= p_ans * np.log(p_ans)
-            if h > max_h:
-                max_h = h
+
+                # h -= p_ans * np.log(p_ans)
+                # 获得P(o|ATCE) = P(o) * P(ATCE|o) / P(ATCE)
+                o_p_post_ans = prior_p * ans_p_post_o / ans_p
+
+                # 质量增益 论文中的 ΔQ(F|T)
+                for i in o_p_post_ans:
+                    if i == 0:  # 有些为0的 不好算log
+                        continue
+                    cur_h -= (i * np.log(i)).item()  # H(o|AS T CE)
+                h_gain = h - cur_h
+            if h_gain > max_h:
+                max_h = h_gain
                 max_selection = selection
-        # print("base: ", max_selection, "max_h: ", max_h)
+        if max_selection == (0,) * num_fact:
+            max_selection = random.sample(range(0, num_fact), num)
+
         return np.array(max_selection), facts.get_subset(list(max_selection)), max_h
 
 
@@ -60,43 +82,75 @@ class GreedyQuerySelector(QuerySelector):  # 改6
     """
     贪心法的问题选择器
     """
-
     def select(self, facts: FactSet,
                num: int,
-               worker_accuracy: np.ndarray) -> Tuple[np.ndarray, "FactSet", float]:
+               worker_accuracy: np.ndarray
+               ) -> Tuple[np.ndarray, "FactSet", float]:
         num_fact: int = facts.num_fact()
         if num > num_fact:  # k等于5，但fact只有4
             num = num_fact
         assert num <= num_fact
         max_selection: list = []
-        # trt_selection: list = [] #不必要
         now_num = 0  # 避免计算len(max_selection)
-        while now_num < num:  # 找到熵最大的fact组合
-            max_h: float = -float('inf')  # 对每次的fact组合取该长度组合的熵最大
+        max_hsum = 0.
+        while now_num < num:  # 近似找到fact最优组合
+            max_h_gain = 0.  # 质量增益最低也得为0
+            # max_h_gain: float = -float('inf')
             max_idx = -1
             for idx in range(num_fact):
-                # print(idx,end=":")
                 if idx in max_selection:
                     continue
                 max_selection.append(idx)
                 sub_facts = facts.get_subset(list(max_selection))
-                h = 0.
-                # 计算answer set的墒
-                # print(max_selection,end=":")
+                prior_p = sub_facts.get_prior_p()  # 初始化prior_p
+                h_gain = 0.
+                if now_num == 0:  # h最初等于H(O)
+                    h = 0.
+                    for i in prior_p:
+                        if i == 0:  # 有些为0的 不好算log
+                            continue
+                        h -= (i * np.log(i)).item()  # H(O)
+                    # h = np.sum(-prior_p * np.log(prior_p)).item()
+                else:  # 后面找的时候 h 需要更新为上次的 H(o|AS T∪{idx} CE)
+                    h = cur_h
                 for i in range(len(sub_facts)):
-                    p_ans, _ = sub_facts.compute_ans_p(sub_facts[i],
+                    cur_h = 0.  # 获得H(o|AS T∪{idx} CE) 即 cur_h
+                    # 传进去sub_facts[i]，相当于一个 o --- (当CE答案为sub_facts[i]时，获得P(ATCE) 和 P(ATCE|o))
+                    ans_p, ans_p_post_o = sub_facts.compute_ans_p(sub_facts[i],
                                                        list(range(sub_facts.num_fact())),
                                                        worker_accuracy[:, list(max_selection)])
-                    h -= p_ans * np.log(p_ans)
-                # print(h)
-                if h > max_h:
-                    max_h = h
+                    # h -= np.sum(ans_p * np.log(ans_p)).item()
+                    # 获得P(o|ATCE) = P(o) * P(ATCE|o) / P(ATCE)
+                    o_p_post_ans = prior_p * ans_p_post_o / ans_p
+
+                    for i in o_p_post_ans:
+                        if i ==0: # 有些为0的 不好算log
+                            continue
+                        cur_h -= (i * np.log(i)).item() # H(o|AS T∪{idx} CE)
+                    # cur_h -= np.sum(-o_p_post_ans * np.log(o_p_post_ans)).item()
+                    h_gain = h - cur_h # 质量增益 每次的gain(f)
+
+                # 选择最大的gain(f)
+                if h_gain > max_h_gain:
+                    max_h_gain = h_gain
                     max_idx = idx   # 每次找出最大的idx在循环外部append
                     # max_selection = copy.copy(max_selection)
                 max_selection.pop(now_num)  # 每次删除的位置一定改用pop()会更快
-            max_selection.append(max_idx)
+            # 不知道为什么有时候max_idx = -1, 而且这好像是导致熵值不降反增的原因 猜测因为这一轮里面所有的 h_gain都小于0
+            # 采取策略是随便塞一个进去
+            if max_idx == -1:
+                # print('max_h:', max_h)
+                for i in range(num_fact):
+                    if i not in max_selection:
+                        max_selection.append(i)
+                        break
+            else:
+                max_hsum += max_h_gain
+                max_selection.append(max_idx)
             now_num += 1
-        return np.array(max_selection), facts.get_subset(list(max_selection)), max_h
+        # print('len(max_selection): ', len(max_selection))
+        # print('max_selection: ', max_selection)
+        return np.array(max_selection), facts.get_subset(list(max_selection)), max_hsum
 
 
 class RandomQuerySelector(QuerySelector):  #2.6
@@ -105,8 +159,7 @@ class RandomQuerySelector(QuerySelector):  #2.6
     """
     def select(self, facts: FactSet,
                num: int,
-               worker_accuracy: np.ndarray
-               ) -> Tuple[np.ndarray, "FactSet", float]:
+               worker_accuracy: np.ndarray) -> Tuple[np.ndarray, "FactSet", float]:
         num_fact: int = facts.num_fact()
         if num > num_fact:
             num = num_fact
